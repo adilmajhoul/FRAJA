a41672de281ec86e34233240459523df409cbbf8
adil mae <itsadilhadid@gmail.com>
2023-11-08 16:44:42 +0100

    i need to figure out why tmdb api fails to respond sometimes

--- src/components/MappedPosterWithPages.jsx
+++ src/components/MappedPosterWithPages.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+
 import axios from "axios";
 import PosterCard from "./posterCard/PosterCard";
 import InfiniteScroll from "react-infinite-scroll-component";
+
 import { useAtom } from "jotai";
 import {
   isTitleFiltering,
@@ -11,93 +13,102 @@ import {
   catchRandomGenre,
   catchRandomPage,
 } from "./search/atoms";
+
 import { englishGenresNameFirst as genres } from "../data/englishGenresNameFirst";
 import { apiKey } from "../data/apiKey";
+
 import randomFetch from "../utils/randomFetch";
 
 export default function MappedPosterWithInfiniteScroll() {
   const [moviesList, setMoviesList] = useState([]);
   const { showType, genre, page } = useParams();
+  const location = useLocation();
   const [loading, setLoading] = useState(true);
 
-  const [randomGenre, setRandomGenre] = useAtom(catchRandomGenre);
-  const [randomPage, setRandomPage] = useAtom(catchRandomPage);
+  // --------------------
 
-  // Initialize random page and genre if not provided
-  useEffect(() => {
-    if (!genre) setRandomGenre(randomFetch("genre"));
-    if (!page) setRandomPage(randomFetch("page"));
-  }, [genre, page]);
-
-  const [currentPage, setCurrentPage] = useState(
-    parseInt(page, 10) || randomPage,
+  // --------------------
+  // set up random page and genre
+  const [randomGenre, setRandomGenre] = useAtom(
+    catchRandomGenre === "" ? randomFetch("genre") : catchRandomGenre,
   );
+  const [randomPage, setRandomPage] = useAtom(
+    catchRandomGenre === "" ? randomFetch("page") : catchRandomGenre,
+  );
+
+  // set up random page and genre
+  useEffect(() => {
+    setRandomPage(randomFetch("page"));
+    setRandomGenre(randomFetch("genre"));
+  }, [page, genre, location]);
+
+  const [currentPage, setCurrentPage] = useState(parseInt(page) || randomPage);
+  // --------------------
+
   const hasMoreData = useRef(true);
 
-  const [titleFiltering] = useAtom(isTitleFiltering);
-  const [minRating] = useAtom(byRatingMin);
-  const [maxRating] = useAtom(byRatingMax);
-
+  const [titleFiltering, setTitleFiltering] = useAtom(isTitleFiltering);
+  // --------------------
+  const [minRating, setMinRating] = useAtom(byRatingMin);
+  const [maxRating, setMaxRating] = useAtom(byRatingMax);
   function ratingFilter(show) {
     return show.vote_average >= minRating && show.vote_average <= maxRating;
   }
-
+  // --------------------
   const fetchData = async () => {
-    setLoading(true);
     try {
-      const genreId = genres[genre] || randomGenre;
-      const pageToFetch = currentPage || randomPage;
-      const response = await axios.get(
+      const res = await axios.get(
         `https://api.themoviedb.org/3/discover/${
           showType || "movie"
-        }?api_key=${apiKey}&sort_by=popularity.desc&with_genres=${genreId}&page=${pageToFetch}`,
+        }?api_key=${apiKey}&sort_by=popularity.desc&with_genres=${
+          genres[genre] || randomGenre
+        }&page=${currentPage}`,
       );
-      const data = response.data.results;
+      const data = res.data.results;
 
       if (data.length > 0) {
         setMoviesList((prevMovies) => [
           ...prevMovies,
-          ...data.filter(ratingFilter),
+          ...data.filter((show) => ratingFilter(show)),
         ]);
-        setCurrentPage((prevPage) => prevPage + 1);
+
+        setCurrentPage(currentPage + 1);
       } else {
         hasMoreData.current = false;
       }
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
     }
   };
-
-  // Effect to reset and fetch data when parameters change
+  // --------------------
   useEffect(() => {
-    setMoviesList([]); // Reset the movies list
-    setCurrentPage(parseInt(page, 10) || 1); // Reset the current page
-    hasMoreData.current = true; // Ensure we can fetch more data
-    fetchData(); // Fetch new data
+    setCurrentPage(parseInt(page) || 1);
+    setMoviesList([]); // Reset the movies list when the genre or page changes
+    hasMoreData.current = true; // Reset hasMoreData to true
+
+    fetchData();
+
+    console.log("genre:", genre, "page:", page, "showType:", showType);
   }, [genre, page, showType, minRating, maxRating]);
-
-  // Loading state UI
-  if (loading) {
-    return <h4 className="text-white">Loading...</h4>;
-  }
-
+  // --------------------
   return (
     <div>
       {!titleFiltering && (
-        <InfiniteScroll
-          dataLength={moviesList.length}
-          next={fetchData}
-          hasMore={hasMoreData.current}
-          loader={<h4 className="text-white">Loading...</h4>}
-        >
-          <div className="flex flex-wrap justify-center">
-            {moviesList.map((show, idx) => (
-              <PosterCard show={show} key={show.id || idx} /> // Use show.id when available for a stable key
-            ))}
-          </div>
-        </InfiniteScroll>
+        <div>
+          <span className="text-white">default shows</span>
+          <InfiniteScroll
+            dataLength={moviesList.length}
+            next={fetchData}
+            hasMore={hasMoreData.current}
+            loader={<h4 className="text-white">Loading...</h4>}
+          >
+            <div className="flex flex-wrap justify-center">
+              {moviesList.map((show, idx) => (
+                <PosterCard show={show} key={show.id || idx} />
+              ))}
+            </div>
+          </InfiniteScroll>
+        </div>
       )}
     </div>
   );
